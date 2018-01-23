74a8bb38f1f3fd01d411912a42596e6b6c626c85
Piotr Miazga <piotr@polishdeveloper.pl>
2018-01-23 22:20:18 +0100

    Use attachments properly

--- index.js
+++ index.js
@@ -55,44 +55,30 @@ app.post('/', (req, res) => {
 		&& req.body.event.channel == "C8WM9Q5FS"
 		&& !req.body.event.bot_id
 	) {
-		console.log( req.body.event.text )
+		console.log( req.body );
 
 		var phabIds = lookupPhabIds( req.body.event.text );
 
 		if (phabIds ) {
 
 			getPhabInfo( phabIds ).then( ticketsData => {
-				//post slack messages
-				// message mock
-				var testMessage = {
-					"text": "I am a test message with with TT183151",
-					"attachments": [
+				var task = ticketsData[phabIds[0]]; //assume only first
+
+				var options = {
+					unfurl_links: false,
+					attachments: [
 						{
-							"title": "Change copy on empty preview",
-							"title_link": "https://phabricator.wikimedia.org/T183151",
-							"text": "Background After discussing the best way to handle previews when we have an error...",
-							"image_url": "https://phab.wmfusercontent.org/file/data/s37v4uvxvzwnsdpsmdux/PHID-FILE-vkbuqfcvjokm45fb6a6q/Screen_Shot_2017-12-19_at_6.23.33_PM.png",
+							"fallback": task.fullname,
+							"title": task.fullname,
+							"title_link": task.uri,
+//							"text": "Background After discussing the best way to handle previews when we have an error...",
+//							"image_url": "https://phab.wmfusercontent.org/file/data/s37v4uvxvzwnsdpsmdux/PHID-FILE-vkbuqfcvjokm45fb6a6q/Screen_Shot_2017-12-19_at_6.23.33_PM.png",
 							"footer": "Have a phabtastic day!",
-							"footer_icon": "https://phab.wmfusercontent.org/res/phabricator/adb05a97/rsrc/favicons/apple-touch-icon-76x76.png",
+//							"footer_icon": "https://phab.wmfusercontent.org/res/phabricator/adb05a97/rsrc/favicons/apple-touch-icon-76x76.png",
 							"fields": [
 								{
-									"title":"Author",
-									"value": "ovasileva",
-									"short": true
-								},
-												{
-									"title":"Assigned to",
-									"value": "ABorbaWMF",
-									"short": true
-								},
-								{
-									"title": "Date",
-									"value": "Dec 18 2017",
-									"short": true
-								},
-								{
-									"title": "Points",
-									"value": 3,
+									"title":"Status",
+									"value": task.status,
 									"short": true
 								}
 							]
@@ -101,7 +87,7 @@ app.post('/', (req, res) => {
 				}; // end attachment mock
 
 				// See: https://api.slack.com/methods/chat.postMessage
-				slackClient.chat.postMessage(req.body.event.channel, testMessage.text, testMessage.attachments)
+				slackClient.chat.postMessage(req.body.event.channel, 'Task details', options)
 				.then((res) => {
 					// `res` contains information about the posted message
   					console.log('Message sent: ', res);
