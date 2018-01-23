76648867b04ae095569abd85d64abc6595ba20a7
Jan Drewniak <jan.drewniak@gmail.com>
2018-01-23 10:11:01 -0800

    Adding express server with Oauth slack strategy

---
+++ index.js
@@ -0,0 +1,29 @@
+const {CLIENT_ID, CLIENT_SECRET, PORT} = process.env,
+      SlackStrategy = require('passport-slack').Strategy,
+      passport = require('passport'),
+      express = require('express'),
+      app = express();
+
+// setup the strategy using defaults
+passport.use(new SlackStrategy({
+    clientID: CLIENT_ID,
+    clientSecret: CLIENT_SECRET
+  }, (accessToken, refreshToken, profile, done) => {
+    // optionally persist profile data
+    done(null, profile);
+  }
+));
+
+app.use(passport.initialize());
+app.use(require('body-parser').urlencoded({ extended: true }));
+
+// path to start the OAuth flow
+app.get('/auth/slack', passport.authorize('slack'));
+
+// OAuth callback url
+app.get('/auth/slack/callback',
+  passport.authorize('slack', { failureRedirect: '/login' }),
+  (req, res) => res.redirect('/')
+);
+
+app.listen(PORT);
