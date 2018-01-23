9660b3aff8402201bea070efffe10f74be9d8369
Piotr Miazga <piotr@polishdeveloper.pl>
2018-01-23 23:37:50 +0100

    Small cleanup

--- index.js
+++ index.js
@@ -9,15 +9,36 @@ app.use(express.json()); // to support JSON-encoded bodies
 app.use(require('body-parser').urlencoded({ extended: true }));
 
 function lookupPhabIds( text ) {
-	if (text === undefined)
-		return [];
-	var idMatches = text.match( /t[0-9]+(\#\w+)?/gi );
-	return idMatches;
+	return text ? text.match( /t[0-9]+(\#\w+)?/gi ) : [];
 }
 
 function lookupGerritIds( text ) {
 	return [];
 }
+
+function createSlackAttachement(title, uri, fields) {
+	var attachment = {
+		fallback: title,
+		title: title,
+		title_link: uri,
+//		text": "Background After discussing the best way to handle previews when we have an error...",
+//		image_url": "https://phab.wmfusercontent.org/file/data/s37v4uvxvzwnsdpsmdux/PHID-FILE-vkbuqfcvjokm45fb6a6q/Screen_Shot_2017-12-19_at_6.23.33_PM.png",
+		footer: "Have a phabtastic day!",
+		footer_icon: "https://phab.wmfusercontent.org/res/phabricator/adb05a97/rsrc/favicons/apple-touch-icon-76x76.png",
+		fields: []
+	};
+	for( var key in fields ) {
+		if (fields.hasOwnProperty(key)) {
+			attachment.fields.push({
+				"title": key,
+				"value": fields[key],
+				"short": true
+			});
+		};
+	}
+	return attachment;
+}
+
 function getPhabInfo( idsWithComments ) {
 	var ids = [];
 	ids = idsWithComments.map( (ticket) => {
@@ -52,46 +73,26 @@ app.post('/', (req, res) => {
 		return;
 	}
 	var type = req.body.event.type;
-        console.log('Message type: ' + type);
 
 	if ( ( type === 'message' || type === 'link_shared' ) && !req.body.event.bot_id ) {
+		console.log(req.body.event);
 		var phabIds = lookupPhabIds( req.body.event.text );
 		console.log('Found phab ids: ', phabIds);
 		if ( phabIds.length > 0 ) {
 			getPhabInfo( phabIds ).then( ticketsData => {
 				var task = ticketsData[phabIds[0]]; //assume only first
-				var options = {
+				slackClient.chat.postMessage(req.body.event.channel, 'Task details', {
 					unfurl_links: false,
-					attachments: [ {
-							"fallback": task.fullName,
-							"title": task.fullName,
-							"title_link": task.uri,
-//							"text": "Background After discussing the best way to handle previews when we have an error...",
-//							"image_url": "https://phab.wmfusercontent.org/file/data/s37v4uvxvzwnsdpsmdux/PHID-FILE-vkbuqfcvjokm45fb6a6q/Screen_Shot_2017-12-19_at_6.23.33_PM.png",
-							"footer": "Have a phabtastic day!",
-//							"footer_icon": "https://phab.wmfusercontent.org/res/phabricator/adb05a97/rsrc/favicons/apple-touch-icon-76x76.png",
-							"fields": [
-								{
-									"title":"Status",
-									"value": task.status,
-									"short": true
-								}
-							]
-						}
+					attachments: [
+						createSlackAttachement( task.fullName, task.uri, { Status: task.status})
 					]
-				}; // end attachment mock
-
-				// See: https://api.slack.com/methods/chat.postMessage
-				slackClient.chat.postMessage(req.body.event.channel, 'Task details', options)
-				.then(() => {
-  					console.log('Message sent');
-				})
-				.catch(console.error);
+				}).then(() => {
+					console.log('Message sent');
+				}).catch(console.error);
 		  });
-		} else {
-		  console.log(req.body);
 		}
 	}
+
 	res.sendStatus(200);
 } );
 
